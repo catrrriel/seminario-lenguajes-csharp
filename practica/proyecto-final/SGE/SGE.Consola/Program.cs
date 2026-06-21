95e8b348b556cdc67fba07968bc1fc6413533142
Catriel <paz.amilcarcatriel@gmail.com>
2026-06-21 18:38:56 -0300

    Implementada DB y terminando Dominio

--- practica/proyecto-final/SGE/SGE.Consola/Program.cs
+++ practica/proyecto-final/SGE/SGE.Consola/Program.cs
@@ -1,16 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SGE.Infraestructura.Datos;
 
-// 1. Configuramos SQLite apuntando a un archivo local
+// Configuramos SQLite apuntando a un archivo local
 var options = new DbContextOptionsBuilder<SgeContext>()
     .UseSqlite("Data Source=SGE.sqlite")
     .Options;
 
-// 2. Instanciamos tu nuevo contexto
+// Instanciamos contexto
 using var context = new SgeContext(options);
 
-// 3. Llamamos a tu inicializador
+// Llamamos al inicializador
 SgeSqlite.Inicializar(context);
 
-Console.WriteLine("¡Base de datos SGE.sqlite generada con éxito!");
+Console.WriteLine("Base de datos SGE.sqlite generada con exito");
 Console.ReadKey();
